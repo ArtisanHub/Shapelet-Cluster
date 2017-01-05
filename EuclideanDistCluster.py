580f7cc2a9ef86b1bedc399783fdcfe7e71882c8
PravindaPerera <pravinda.joes@gmail.com>
2017-01-05 20:02:53 +0530

    Modified clustering approach

--- EuclideanDistCluster.py
+++ EuclideanDistCluster.py
@@ -16,7 +16,7 @@ f = open('D:/FYP-Developments/Shapelet-Cluster/occu_demo_data.csv', 'rU' ) #open
 #preping the analysis file with the respective window data
 for line in f:
     cells = line.split(",")
-    # print("line number:"  + str(count1) + " ########### " + cells[7])
+    print("line number:"  + str(count1))
 
     k = open('D:/FYP-Developments/Shapelet-Cluster/occu_demo_data.csv', 'rU')  # open train data
 
@@ -24,19 +24,20 @@ for line in f:
 
     for tempLine in k:
         tempCells = tempLine.split(",")
-        distance = pow((float(cells[2]) - float(tempCells[2])), 2) + pow((float(cells[3]) - float(tempCells[3])), 2) + pow((float(cells[4]) - float(tempCells[4])), 2) + pow((float(cells[5]) - float(tempCells[5])), 2) + pow((float(cells[6]) - float(tempCells[6])), 2)
+        distance = pow(pow((float(cells[2]) - float(tempCells[2])), 2) + pow((float(cells[3]) - float(tempCells[3])), 2) + pow((float(cells[4]) - float(tempCells[4])), 2) + pow((float(cells[5]) - float(tempCells[5])), 2) + pow((float(cells[6]) - float(tempCells[6])), 2),0.5)
         tempDistances.append(distance)
         #print(str(distance) + " $$$$$$$$ " + str(tempCells[7]))
 
-
     k.close()
+
+    print(tempDistances)
 
     count2 = 1
     status = True
     for x in tempDistances:
 
         if count2 not in clusterdRowID:
-            if x <= 15000:
+            if x <= 600:
                 clusterdRowID.append(count2)
                 if status:
                     eventType = eventType + 1
@@ -48,6 +49,7 @@ for line in f:
 
         count2 = count2 + 1
 
+        #print(tempDistances)
     count1 = count1 + 1
 
 f.close()
